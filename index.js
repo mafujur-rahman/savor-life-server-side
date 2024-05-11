9bdf448cdde84725f6934ab459d7731e951c8f4c
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-05-11 13:09:25 +0600

    Added env variable in git ignore file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,13 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express())
 
-// blogMaster
-// hQNrFtnBxtdZZprs
 
 
-
-
-const uri = "mongodb+srv://blogMaster:hQNrFtnBxtdZZprs@cluster0.zjwopdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjwopdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {

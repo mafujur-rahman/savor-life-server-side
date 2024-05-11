7490033d08810cab3c6076ee21f2f38d49c7f933
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-05-11 14:11:49 +0600

    Added post method

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // midleware
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 
@@ -15,32 +15,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const blogCollection = client.db('blogDB').collection('blog');
+
+        app.post('/blogs', async (req, res) => {
+            const newBlog = req.body;
+            console.log(newBlog)
+            const result = await blogCollection.insertOne(newBlog);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send("savor life server is running")
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`server connected: ${port}`);
 })

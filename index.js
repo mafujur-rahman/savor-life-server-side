35674a4a168db1df3e5eff74febef4de3b09b061
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-05-12 14:10:48 +0600

    get comments data from database

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/comments', async(req, res) =>{
+            const cursor = commentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
         app.get('/blogs/:id', async(req, res) =>{
             const id = req.params.id;
             const query = {_id : new ObjectId(id)}
